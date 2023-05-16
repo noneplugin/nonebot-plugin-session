601b37e3c4afede27f24e6eb3280e8e477098e6a
meetwq <meetwq@gmail.com>
2023-05-16 10:35:26 +0800

    add datastore model

---
+++ nonebot_plugin_session/model.py
@@ -0,0 +1,92 @@
+from typing import Optional, Union
+
+from .session import Session, SessionIdType, SessionLevel
+
+try:
+    from nonebot_plugin_datastore import create_session, get_plugin_data
+    from sqlalchemy import Enum, String, UniqueConstraint, select
+    from sqlalchemy.ext.hybrid import hybrid_method
+    from sqlalchemy.orm import Mapped, mapped_column
+
+    Model = get_plugin_data().Model
+
+    class SessionModel(Model):
+        __table_args__ = {
+            UniqueConstraint(
+                "bot_id",
+                "bot_type",
+                "platform",
+                "level",
+                "id1",
+                "id2",
+                "id3",
+                name="unique_session",
+            ),
+        }
+
+        id: Mapped[int] = mapped_column(primary_key=True)
+        bot_id: Mapped[str] = mapped_column(String(64))
+        bot_type: Mapped[str] = mapped_column(String(32))
+        platform: Mapped[str] = mapped_column(String(32))
+        level: Mapped[SessionLevel] = mapped_column(Enum)
+        id1: Mapped[Optional[str]] = mapped_column(String(64))
+        id2: Mapped[Optional[str]] = mapped_column(String(64))
+        id3: Mapped[Optional[str]] = mapped_column(String(64))
+
+        @hybrid_method
+        def get_id(
+            self,
+            id_type: Union[int, SessionIdType],
+            *,
+            include_platform: bool = True,
+            include_bot_type: bool = True,
+            include_bot_id: bool = True,
+            seperator: str = "_",
+        ) -> str:
+            return Session(
+                bot_id=self.bot_id,
+                bot_type=self.bot_type,
+                platform=self.platform,
+                level=self.level,
+                id1=self.id1,
+                id2=self.id2,
+                id3=self.id3,
+            ).get_id(
+                id_type=id_type,
+                include_bot_id=include_bot_id,
+                include_bot_type=include_bot_type,
+                include_platform=include_platform,
+                seperator=seperator,
+            )
+
+    async def get_or_create_session_model(session: Session) -> SessionModel:
+        async with create_session() as db_session:
+            statement = (
+                select(SessionModel)
+                .where(SessionModel.bot_id == session.bot_id)
+                .where(SessionModel.bot_type == session.bot_type)
+                .where(SessionModel.platform == session.platform)
+                .where(SessionModel.level == session.level)
+                .where(SessionModel.id1 == session.id1)
+                .where(SessionModel.id2 == session.id2)
+                .where(SessionModel.id3 == session.id3)
+            )
+            results = await db_session.scalars(statement)
+            if session_model := results.one_or_none():
+                return session_model
+
+            session_model = SessionModel(
+                bot_id=session.bot_id,
+                bot_type=session.bot_type,
+                platform=session.platform,
+                level=session.level,
+                id1=session.id1,
+                id2=session.id2,
+                id3=session.id3,
+            )
+            db_session.add(session_model)
+            await db_session.commit()
+            return session_model
+
+except ImportError:
+    pass
