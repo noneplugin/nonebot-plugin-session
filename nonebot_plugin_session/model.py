67953858acd5fa3d10e2a5d02db31ac13b3c20bc
ssttkkl <huang.wen.long@hotmail.com>
2023-06-02 09:14:54 +0800

    support incoming db_session when invoke get_or_add_session_model

--- nonebot_plugin_session/model.py
+++ nonebot_plugin_session/model.py
@@ -1,4 +1,7 @@
-from typing import Optional
+from contextlib import asynccontextmanager
+from typing import Optional, AsyncContextManager
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from .session import Session, SessionLevel
 
@@ -48,8 +51,24 @@ try:
                 id3=self.id3,
             )
 
-    async def get_or_add_session_model(session: Session) -> SessionModel:
-        async with create_session() as db_session:
+
+    @asynccontextmanager
+    async def _use_or_create_session(session: Optional[AsyncSession]) -> AsyncContextManager[AsyncSession]:
+        session_provided = session is not None
+        if not session_provided:
+            session = create_session()
+
+        try:
+            yield session
+        finally:
+            if not session_provided:
+                await session.close()
+
+
+    async def get_or_add_session_model(session: Session, db_session: Optional[AsyncSession] = None,
+                                       commit: bool = True) -> SessionModel:
+        session_provided = db_session is not None
+        async with _use_or_create_session(db_session) as db_session:
             statement = (
                 select(SessionModel)
                 .where(SessionModel.bot_id == session.bot_id)
@@ -74,7 +93,9 @@ try:
                 id3=session.id3,
             )
             db_session.add(session_model)
-            await db_session.commit()
+            if not session_provided or commit:
+                await db_session.commit()
+                await db_session.refresh(session_model)
             return session_model
 
 except (ImportError, RuntimeError):
