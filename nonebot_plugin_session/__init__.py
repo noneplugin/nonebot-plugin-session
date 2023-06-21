36a276720eb5396e487344bc6db1361edb9bb125
meetwq <meetwq@gmail.com>
2023-06-21 11:15:12 +0800

    add metadata

--- nonebot_plugin_session/__init__.py
+++ nonebot_plugin_session/__init__.py
@@ -1,3 +1,21 @@
+from nonebot.plugin import PluginMetadata
+
 from . import adapters, model, saa
 from .extractor import SessionExtractor, SessionId, extract_session
 from .session import Session, SessionIdType, SessionLevel
+
+__plugin_meta__ = PluginMetadata(
+    name="会话id",
+    description="会话信息提取与会话id定义插件",
+    usage="请参考文档",
+    type="library",
+    homepage="https://github.com/noneplugin/nonebot-plugin-session",
+    supported_adapters={
+        "~onebot.v11",
+        "~onebot.v12",
+        "~console",
+        "~kaiheila",
+        "~qqguild",
+        "~telegram",
+    },
+)
