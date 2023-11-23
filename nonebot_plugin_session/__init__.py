43cd01edd92856d9bce7f7e0c2c4b58c6fb5d3d0
meetwq <meetwq@gmail.com>
2023-11-23 15:04:30 +0800

    make ruff happy

--- nonebot_plugin_session/__init__.py
+++ nonebot_plugin_session/__init__.py
@@ -1,5 +1,14 @@
 from nonebot.plugin import PluginMetadata
 
+from . import adapters as adapters
+from .extractor import EventSession as EventSession
+from .extractor import SessionExtractor as SessionExtractor
+from .extractor import SessionId as SessionId
+from .extractor import extract_session as extract_session
+from .session import Session as Session
+from .session import SessionIdType as SessionIdType
+from .session import SessionLevel as SessionLevel
+
 __plugin_meta__ = PluginMetadata(
     name="会话id",
     description="会话信息提取与会话id定义插件",
--- nonebot_plugin_session/adapters/__init__.py
+++ nonebot_plugin_session/adapters/__init__.py
@@ -1,15 +1,13 @@
-from . import (  # noqa F401
-    console,
-    discord,
-    dodo,
-    feishu,
-    kaiheila,
-    onebot_v11,
-    onebot_v12,
-    qq,
-    qqguild,
-    red,
-    satori,
-    telegram,
-    villa,
-)
+from . import console as console
+from . import discord as discord
+from . import dodo as dodo
+from . import feishu as feishu
+from . import kaiheila as kaiheila
+from . import onebot_v11 as onebot_v11
+from . import onebot_v12 as onebot_v12
+from . import qq as qq
+from . import qqguild as qqguild
+from . import red as red
+from . import satori as satori
+from . import telegram as telegram
+from . import villa as villa
