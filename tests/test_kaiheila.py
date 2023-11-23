d56b9de015f4454f849566f9b2a699f02978e043
meetwq <meetwq@gmail.com>
2023-11-23 15:39:49 +0800

    add additional tests

--- tests/test_kaiheila.py
+++ tests/test_kaiheila.py
@@ -138,6 +138,67 @@ def test_channel_message_event(app: App):
     )
 
 
+def test_server_message(app: App):
+    from nonebot_plugin_session import SessionLevel, extract_session
+
+    bot = new_bot(self_id="2233")
+    event = ChannelMessageEvent(
+        post_type="message",
+        channel_type="GROUP",
+        type=255,
+        target_id="6677",
+        author_id="3344",
+        content="[系统消息]",
+        msg_id="4455",
+        msg_timestamp=1234,
+        nonce="",
+        extra=Extra(
+            type=1,
+            guild_id="5566",
+            channel_name="test",
+            mention=[],
+            mention_all=False,
+            mention_roles=[],
+            mention_here=False,
+            author=None,
+            body=None,
+            attachments=None,
+            code=None,
+        ),
+        user_id="3344",
+        self_id="2233",
+        group_id="6677",
+        message_type="group",
+        sub_type="",
+        event=EventMessage(
+            type=1,
+            guild_id="5566",
+            channel_name="test",
+            content=Message("[系统消息]"),
+            mention=[],
+            mention_all=False,
+            mention_roles=[],
+            mention_here=False,
+            nav_channels=[],
+            author=User(id="3344"),
+            kmarkdown=None,
+            attachments=None,
+            code=None,
+        ),
+    )
+    session = extract_session(bot, event)
+    assert_session(
+        session,
+        bot_id="2233",
+        bot_type="Kaiheila",
+        platform="kaiheila",
+        level=SessionLevel.LEVEL3,
+        id1="3344",
+        id2=None,
+        id3="6677",
+    )
+
+
 def test_undefined_event(app: App):
     from nonebot_plugin_session import SessionLevel, extract_session
 
