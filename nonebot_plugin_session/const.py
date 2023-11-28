4a328ecd9c9e116d21a0f400567d10ba0edc7088
meetwq <meetwq@gmail.com>
2023-11-28 13:41:01 +0800

    fix: add SupportedAdapter

--- nonebot_plugin_session/const.py
+++ nonebot_plugin_session/const.py
@@ -1,6 +1,20 @@
 from strenum import StrEnum
 
 
+class SupportedAdapter(StrEnum):
+    onebot_v11 = "OneBot V11"
+    onebot_v12 = "OneBot V12"
+    console = "Console"
+    kaiheila = "Kaiheila"
+    qqguild = "QQ Guild"
+    telegram = "Telegram"
+    feishu = "Feishu"
+    red = "RedProtocol"
+    discord = "Discord"
+    villa = "Villa"
+    dodo = "DoDo"
+
+
 class SupportedPlatform(StrEnum):
     qq = "qq"
     console = "console"
